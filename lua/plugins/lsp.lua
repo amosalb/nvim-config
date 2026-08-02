d813afd3c2f97b1150afb4c07de7a0ca8b23ed7d
Amós Albuquerque <amos.a.albuquerque@gmail.com>
2026-08-02 16:43:21 -0300

    Inclui linguagens a serem instalads

--- lua/plugins/lsp.lua
+++ lua/plugins/lsp.lua
@@ -1,7 +1,9 @@
 return {
     {
         "mason-org/mason-lspconfig.nvim",
-        opts = {},
+        opts = {
+            ensure_installed = { "lua_ls", "bashls" },
+        },
         dependencies = {
             { "mason-org/mason.nvim", opts = {} },
             "neovim/nvim-lspconfig",
