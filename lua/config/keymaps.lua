5a2614dfc56b61c58cd99c3ba332bd0807a7b559
Amós Albuquerque <84880072+amosalb@users.noreply.github.com>
2026-08-02 15:31:35 -0300

    Add files via upload

---
+++ lua/config/keymaps.lua
@@ -0,0 +1,16 @@
+-- Personal keymaps
+vim.keymap.set("n", "<leader>w", ":w<CR>")
+vim.keymap.set("n", "<leader>q", ":q<CR>", { desc = 'Quit' })
+vim.keymap.set({"i","v"}, "jk", "<Esc>", { desc = 'Escape' })
+
+-- Lazy
+vim.keymap.set("n", "<leader>l", ":Lazy<CR>", { desc = 'Lazy' })
+
+-- Telescope
+vim.keymap.set('n', '<leader>ff', function() require('telescope.builtin').find_files() end, { desc = 'Telescope find files' })
+vim.keymap.set('n', '<leader>fg', function() require('telescope.builtin').live_grep() end, { desc = 'Telescope live grep' })
+vim.keymap.set('n', '<leader>fb', function() require('telescope.builtin').buffers() end, { desc = 'Telescope buffers' })
+vim.keymap.set('n', '<leader>fh', function() require('telescope.builtin').help_tags() end, { desc = 'Telescope help tags' })
+
+-- Oil
+vim.keymap.set("n", "-", "<CMD>Oil<CR>", { desc = "Open parent directory" })
---
+++ lua/config/options.lua
@@ -0,0 +1,13 @@
+local opt = vim.opt
+
+opt.number = true
+opt.relativenumber = true
+opt.tabstop = 4
+opt.shiftwidth = 4
+opt.expandtab = true
+opt.smartindent = true
+opt.wrap = false
+opt.ignorecase = true
+opt.smartcase = true
+opt.termguicolors = true
+opt.clipboard = "unnamedplus"
