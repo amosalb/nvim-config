9c7486d1159e612d57c7f38099c3d578286cce9d
Amós Albuquerque <amos.a.albuquerque@gmail.com>
2026-08-03 12:34:38 -0300

    Inclui lsp pyright e ruff

--- lua/plugins/lsp.lua
+++ lua/plugins/lsp.lua
@@ -2,7 +2,12 @@ return {
     {
         "mason-org/mason-lspconfig.nvim",
         opts = {
-            ensure_installed = { "lua_ls", "bashls" },
+            ensure_installed = {
+                "lua_ls",
+                "bashls",
+                "pyright",
+                "ruff"
+            },
         },
         dependencies = {
             { "mason-org/mason.nvim", opts = {} },
