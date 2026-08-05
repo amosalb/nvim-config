bd9061db82a10a796b312b220b0bf315aceea9f6
Amós Albuquerque <amos.a.albuquerque@gmail.com>
2026-08-05 10:35:46 -0300

    Inclui exibição de diagnóstico onHover do cursor

--- lua/plugins/lsp.lua
+++ lua/plugins/lsp.lua
@@ -1,18 +1,31 @@
 return {
-    {
-        "mason-org/mason-lspconfig.nvim",
-        opts = {
-            ensure_installed = {
-                "lua_ls",
-                "bashls",
-                "pyright",
-                "ruff",
-                "stylua",
-            },
-        },
-        dependencies = {
-            { "mason-org/mason.nvim", opts = {} },
-            "neovim/nvim-lspconfig",
-        },
-    }
+	{
+		"mason-org/mason-lspconfig.nvim",
+		opts = {
+			ensure_installed = {
+				"lua_ls",
+				"bashls",
+				"pyright",
+				"ruff",
+				"stylua",
+			},
+		},
+		dependencies = {
+			{ "mason-org/mason.nvim", opts = {} },
+			{
+				"neovim/nvim-lspconfig",
+				config = function()
+					-- Show diagnostics under the cursor when holding position
+					vim.api.nvim_create_autocmd("CursorHold", {
+						callback = function()
+							vim.diagnostic.open_float(nil, { focus = false })
+						end,
+					})
+
+					-- Decrease update time from default 4000ms to 300ms for a faster response
+					vim.o.updatetime = 300
+				end,
+			},
+		},
+	},
 }
