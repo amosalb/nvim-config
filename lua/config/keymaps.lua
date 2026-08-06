a1d3b690f5bf89445cd78e91225b8309e44cb168
Amós Albuquerque <amos.a.albuquerque@gmail.com>
2026-08-06 18:53:22 -0300

    Altera o mapeamento do telescope para arquivo do plugin e inclui keymap para fechamento de buffer

--- lua/config/keymaps.lua
+++ lua/config/keymaps.lua
@@ -1,19 +1,7 @@
--- Personal keymaps
 vim.keymap.set("n", "<leader>w", ":w<CR>")
 vim.keymap.set("n", "<leader>q", ":q<CR>", { desc = "Quit" })
 vim.keymap.set({ "i", "v" }, "jk", "<Esc>", { desc = "Escape" })
+vim.keymap.set("n", "<leader>bd", "<cmd>bd<CR>", { desc = "Delete current buffer" })
 
--- Telescope
-vim.keymap.set("n", "<leader>ff", function()
-	require("telescope.builtin").find_files()
-end, { desc = "Telescope find files" })
-vim.keymap.set("n", "<leader>fg", function()
-	require("telescope.builtin").live_grep()
-end, { desc = "Telescope live grep" })
-vim.keymap.set("n", "<leader>fb", function()
-	require("telescope.builtin").buffers()
-end, { desc = "Telescope buffers" })
-vim.keymap.set("n", "<leader>fh", function()
-	require("telescope.builtin").help_tags()
-end, { desc = "Telescope help tags" })
+
 
